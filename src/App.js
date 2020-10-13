1314df263a9dfc5e7bef04ace86be4400f1c8269
Samuel Martensson <samuel137@live.se>
2020-10-13 12:44:49 +0200

    wrapped up some styles

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import StartScreen from "./components/StartScreen";
 import { QuizContext } from "./provider/QuizProvider";
 import { Switch, Route } from "react-router-dom";
 import QuizSetupScreen from "./components/QuizSetupScreen";
+import Quiz from "./components/Quiz";
 
 function App() {
   const { currentGame, setCurrentGame } = useContext(QuizContext);
   return (
     <div>
       <Switch>
+        <Route path="/quiz">
+          <Quiz />
+        </Route>
         <Route path="/setup">
-          <QuizSetupScreen />
+          <QuizSetupScreen setCurrentGame={setCurrentGame} />
         </Route>
         <Route path="/">
-          <StartScreen currentGame={currentGame} />
+          <StartScreen />
         </Route>
       </Switch>
     </div>
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Quiz = () => {
+  return <div>QUIZ!</div>;
+};
+
+export default Quiz;
