cd64124afee18f498a061fe4207b0217f21a01d3
Patric Ronge <pronge@gmail.com>
2020-10-13 10:31:03 +0200

    Added styled-components, theme, and dark-mode

--- src/provider/QuizProvider.js
+++ src/provider/QuizProvider.js
@@ -1,13 +1,20 @@
 import React, { createContext, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../styled/GlobalStyles";
+import { theme } from "../styled/theme";
 
 export const QuizContext = createContext();
 
 const QuizProvider = ({ children }) => {
   const [currentGame, setCurrentGame] = useState({});
+  const [darkTheme, setDarkTheme] = useState(true);
 
   return (
     <QuizContext.Provider value={{ currentGame, setCurrentGame }}>
-        {children}
+      <ThemeProvider theme={darkTheme ? theme.dark : theme.light}>
+        <GlobalStyles />
+          {children}
+      </ThemeProvider>
     </QuizContext.Provider>
   );
 };
